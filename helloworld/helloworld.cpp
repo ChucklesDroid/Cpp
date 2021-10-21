e132a6ece67fb83e7d9f3297c54036d3f4c95f12
ChuclesDroid <mj.akarsh@gmail.com>
2021-10-22 02:42:02 +0530

    Helloworld Program

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // library to include standard input and output streams for C++ language
+#include<cstdio>   // library to include standard C functions in C++ ( cstdio.h is C alternative )	
+
+using namespace std ; //
+int main( int argc , char *argv[] )
+{
+	char ch[20] = "Hello , World!" ;
+	//printf("Hello , World !") ;
+	cout<<"Hello , World !";
+	return 0 ;
+}
