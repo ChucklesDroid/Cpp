04c781f499a9c97c0d5ab323487cdbdce7a7c009
ChuclesDroid <mj.akarsh@gmail.com>
2022-01-19 10:11:34 +0530

    Added variable scope and data types

---
+++ C++ online course/variableScopeDataTypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int glo=6 ;
+void sum( int glo )
+{
+    cout<<glo ;
+}
+int main( int argc , char *argv[] )
+{
+    int glo = 78 ;
+    glo = 23 ;
+/* Local variable passed by argument */
+    sum( glo ) ;
+/* Local Variable printed */
+    cout<<endl<<glo ;
+/* Global Variable printed */
+    cout<<endl<<::glo<<endl ;
+    // int a = 4 , b = 5 ;
+    // float pi = 3.14 ;
+    // char c = 'd'; 
+    // cout<<"Value of a is "<<a<<"\nValue of b is "<<b<<endl ;
+    // cout<<"Value of a is "<<a<<"\nValue of pi is "<<pi<<endl ;
+    // cout<<"Value of c is "<<c<<endl;
+    return 0 ;
+}
