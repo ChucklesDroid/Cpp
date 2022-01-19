fa2aa3109bb653da328eff25e3c6e2cdace87e7e
ChuclesDroid <mj.akarsh@gmail.com>
2022-01-19 10:11:12 +0530

    Added comments & variables

---
+++ C++ online course/Variables&comments.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+
+int main( int argc, char *argv[] )
+{   
+// Single Line Comment
+/* Multi
+    Line 
+     Comment */
+    int sum= 6 ; // Declaration and initialisation
+    cout<<"Hello World"<<sum ;
+    return 0 ;
+}
