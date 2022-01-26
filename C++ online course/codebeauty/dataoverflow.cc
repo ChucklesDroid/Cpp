e64129f9191e762eabae45e84f6f46c8d110440f
ChucklesDroid <mj.akarsh@gmail.com>
2022-01-26 16:47:21 +0530

    Following online tut for learning about dsa

---
+++ C++ online course/codebeauty/dataoverflow.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main( int argc, char *argv[] )
+{
+    std::cout<<INT64_MAX<<std::endl ;
+/* Data overflowed and value represented is signed representation */
+    std::cout<<INT64_MAX<<std::endl ;
+    return 0;
+}
