1de104c8c09600420a3764c97b47332be99db20a
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-01 16:58:07 +0530

    reconciled with local library

--- C++ online course/educative/Literals/Examples/builtIn.cc
+++ C++ online course/educative/Literals/Examples/builtIn.cc
@@ -12,7 +12,14 @@ int main( int argc, char *argv[] )
     auto schoolWay = 25min;
     auto homework = 2h;
     
-    auto schoolDayinHours = (schoolHour*6) + (shortBreak*4) + (longBreak*1) + (schoolWay*2) + homework ;
+    auto schoolDayinSecs = (schoolHour*6) + (shortBreak*4) + (longBreak*1) + (schoolWay*2) + homework ;
+   std::chrono::duration<double, std::ratio<3600>> schoolDayinHrs = schoolDayinSecs;
+   std::chrono::duration<double, std::ratio<60>> schoolDayinMins = schoolDayinSecs;
+   std::chrono::duration<double, std::ratio<1,1000>> schoolDayinMilliSecs = schoolDayinSecs;
 
+   std::cout<<"School Day in Hours :"<<schoolDayinHrs.count()<<"h"<<std::endl;
+   std::cout<<"School Day in Mins :"<<schoolDayinMins.count()<<"mins"<<std::endl;
+   std::cout<<"School Day in Secs :"<<schoolDayinSecs.count()<<"s"<<std::endl;
+   std::cout<<"School Day in Milli Secs :"<<schoolDayinMilliSecs.count()<<"ms"<<std::endl;
     return 0 ;
 }
