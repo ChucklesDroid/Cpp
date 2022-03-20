b6862810c4f2a9b33169bdc717ecefc926685c2d
ChucklesDroid <mj.akarsh@gmail.com>
2022-03-20 11:37:43 +0530

    modified the soln according to question

--- C++Primer/Chapter3/Sol3_07.cc
+++ C++Primer/Chapter3/Sol3_07.cc
@@ -11,20 +11,29 @@ int main( int argc, char *argv[] )
 	string s1 ;
 	string s2 ;
 	cin >> s1 >> s2 ;
-	if( s1 == s2 ){
-		cout << "They are of equal \n" ;
+	// if( s1 == s2 ){
+		// cout << "They are of equal \n" ;
+	// } else {
+		// string::size_type maxLength = (s1.length() > s2.length() ? s2.length() : s1.length()) ;
+		// for( string::size_type index = 0; index < maxLength; index++ ){
+			// if( s1[index] > s2[index] ){
+				// cout << "s1 is larger than s2: " << s1[index] - s2[index] ;
+				// return 0 ;
+			// }	else if ( s1[index] < s2[index] ){
+				// cout << "s2 is larger than s1: " << s2[index] - s1[index] ;
+				// return 0 ;
+			// } else {
+				// continue ;
+			// }
+		// }
+	// }
+	if ( s1.length() == s2.length() ){
+		cout << "They are of equal length" ;
 	} else {
-		string::size_type maxLength = (s1.length() > s2.length() ? s2.length() : s1.length()) ;
-		for( string::size_type index = 0; index < maxLength; index++ ){
-			if( s1[index] > s2[index] ){
-				cout << "s1 is larger than s2: " << s1[index] - s2[index] ;
-				return 0 ;
-			}	else if ( s1[index] < s2[index] ){
-				cout << "s2 is larger than s1: " << s2[index] - s1[index] ;
-				return 0 ;
-			} else {
-				continue ;
-			}
+		if( s1.length() > s2.length() ){
+			cout << s1 << " :is greater in length" ;
+		} else {
+			cout << s2 << " :is greater in length" ;
 		}
 	}
 	return 0 ;
